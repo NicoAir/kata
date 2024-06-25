43fa3a5897f643b7032855f653aed2b79f1bbdef
Nico <n-augier@hotmail.fr>
2024-06-25 14:29:03 +0200

    impl

--- kata2.py
+++ kata2.py
@@ -10,7 +10,9 @@ def add(numbers: str) -> int:
         numbers_list = re.split(re.escape(delimiter), numbers)
     else:
         numbers_list = re.split(delimiter, numbers)
+    numbers_list = [char if element == '' else char for element in numbers_list for char in (list(element) if element != '' else [''])] 
     for num in numbers_list:
         if not num.isdigit():
-            raise ValueError
+                position = numbers.find(num)
+                raise ValueError(f"'{delimiter}' expected but '{num}' found at position {position}.")
     return sum(int(num) for num in numbers_list if num.isdigit())
