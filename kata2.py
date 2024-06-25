9a39340a6d95b21fd0df039ac9e41c133c08f830
Nico <n-augier@hotmail.fr>
2024-06-25 14:02:53 +0200

    impl

--- kata2.py
+++ kata2.py
@@ -1,5 +1,7 @@
+import re
+
 def add(numbers: str) -> int:
     if not numbers:
         return 0
-    numbers_list = numbers.split(",")
+    numbers_list = re.split(',|\n', numbers)
     return sum([int(number) for number in numbers_list])
