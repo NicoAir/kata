5caa8cfd3163fc0487e5b1ba5d4e892e68fdb0f5
Nico <n-augier@hotmail.fr>
2024-06-25 14:05:04 +0200

    add bad separation

--- test_kata2.py
+++ test_kata2.py
@@ -1,4 +1,5 @@
 from kata2 import add
+from pytest import raises
 
 def testAddZero():
     assert add("") == 0
@@ -12,3 +13,7 @@ def testAddTwo():
 def testAddThree():
     assert add("1,2,3") == 6
     assert add("1,2\n3") == 6
+
+def testAddBadSeparation():
+    with raises(ValueError):
+        add("2,\n3")
