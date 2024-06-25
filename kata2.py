22b21d1b7f23d7d56f21020f9ff19368ad5f53c1
Nico <n-augier@hotmail.fr>
2024-06-25 14:13:03 +0200

    impl

--- kata2.py
+++ kata2.py
@@ -3,5 +3,14 @@ import re
 def add(numbers: str) -> int:
     if not numbers:
         return 0
-    numbers_list = re.split(',|\n', numbers)
-    return sum([int(number) for number in numbers_list])
+    delimiter = ',|\n'
+    if numbers.startswith('//'):
+        delimiter, numbers = numbers.split('\n', 1)
+        delimiter = delimiter[2:]
+        numbers_list = re.split(re.escape(delimiter), numbers)
+    else:
+        numbers_list = re.split(delimiter, numbers)
+    for num in numbers_list:
+        if not num.isdigit():
+            raise ValueError
+    return sum(int(num) for num in numbers_list if num.isdigit())
